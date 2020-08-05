a4e43d89c0451bc6da8100175fe85dd7e961f9f4
MaartenvanderMeulen <maarten.vandermeulen@gmail.com>
2020-08-05 08:46:22 +0200

    improved writing of code to file

--- search.py
+++ search.py
@@ -1,27 +1,87 @@
 '''Search for new functions'''
 import sys
 import interpret
+import evaluate
 
 
 def is_solved_by_function(examples, fname, functions):
     for actual_params, expected_output in examples:
-        fcall = [fname] + actual_params
-        actual_output = interpret.call_function(fcall, dict(), functions, False, "")
+        code = [fname] + actual_params
+        variables = dict()
+        actual_output = interpret.run(code, variables, functions)
         if actual_output != expected_output:
-            return False        
+            return False
     return True
 
 
 def solve_by_existing_function(problem, functions):
     problem_label, examples = problem
+    build_in_functions = interpret.get_build_in_functions()
+    for fname in build_in_functions:
+        if is_solved_by_function(examples, fname, dict()):
+            return fname
     for fname, (params, code) in functions.items():
         if is_solved_by_function(examples, fname, functions):
             return fname
     return None
-   
+
+
+def create_random_function_name(functions):
+    i = 0
+    while f"f{i}" in functions:
+        i += 1
+    return f"f{i}"
+    
+    
+def create_random_code(functions):
+    # TODO
+    return params, code
+
+
+def create_random_function(functions):
+    stub = True
+    if stub:
+        return ["add_ab1", ["a", "b"], ["add", "a", "b", 1]]
+    fname = create_random_function_name(functions)
+    params, code = create_random_code(functions)
+    return [fname, params, code]
+    
+    
+def compute_error(function, problem, functions):
+    result = 0.0
+    fname, formal_params, code = function
+    problem_label, examples = problem
+    for actual_params, expected_output in examples:
+        variables = interpret.bind_params(formal_params, actual_params)
+        actual_output = interpret.run(code, variables, functions)
+        result += evaluate.evaluate(actual_output, expected_output, False)
+    return result
+
+
+def room_for_improvement(error):
+    return error[-1] > 0 and (len(error) == 1 or error[-2] > error[-1])
+    
+    
+def local_search(function, problem, functions):
+    initial_error = compute_error(function, problem, functions)
+    
+    # TODO : do the local search
+    improved_function = function
+    
+    final_error = compute_error(improved_function, problem, functions)
+    assert final_error <= initial_error
+    return final_error
+
 
 def solve_by_new_function(problem, functions):
-    # TODO
+    for hop in range(10):
+        function = create_random_function(functions)
+        error = [compute_error(function, problem, functions)]
+        while room_for_improvement(error):
+            error.append(local_search(function, problem, functions))
+        if error[-1] == 0:
+            return function
+        print(problem[0], "hop", hop, "error", error[-1])
     return None
 
 
