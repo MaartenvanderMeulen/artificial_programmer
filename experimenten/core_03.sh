90aa738e63e9a6be541f625cc11083d0797309c4
Maarten van der Meulen <maarten.vandermeulen@gmail.com>
2020-11-12 19:38:33 +0100

    start is_magic_square at level 2 with larger search space

---
+++ experimenten/core_03.sh
@@ -0,0 +1,25 @@
+/c/Users/Maarten/Miniconda3/python search.py 103 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 107 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 111 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 115 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 119 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 123 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 127 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 131 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 135 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 139 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 143 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 147 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 151 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 155 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 159 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 163 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 167 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 171 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 175 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 179 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 183 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 187 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 191 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 195 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 199 tmp/$1 experimenten/params_$1.txt
--- search.py
+++ search.py
@@ -1,12 +1,14 @@
 '''Search for new functions.  Usage:
 python search.py [functions.txt [problems.txt]]
 '''
+import os
 import sys
 import time
 import interpret
 import evaluate
 import random
 import numpy as np
+import json
 import ga_search_deap
 import evaluate
 
@@ -34,8 +36,9 @@ def solve_by_existing_function(problem, functions):
     return None
 
 
-def find_new_functions(problems, functions, layer, f, verbose, append_functions_to_file=None):
+def find_new_functions(problems, functions, layer, f, params, append_functions_to_file=None):
     '''If append_functions_to_file is a string, the new functions will be appended to that file'''
+    verbose = params["verbose"]
     if verbose > 0:
         f.write(f"Solving problems, layer {layer} ...\n")
     new_functions = []
@@ -49,7 +52,7 @@ def find_new_functions(problems, functions, layer, f, verbose, append_functions_
             else:
                 if verbose > 0:
                     f.write(f"problem  {problem_label} ...\n")
-                function_code = ga_search_deap.solve_by_new_function(problem, functions, f, 1)
+                function_code = ga_search_deap.solve_by_new_function(problem, functions, f, params)
                 if function_code:
                     function_str = interpret.convert_code_to_str(function_code)
                     new_functions.append(function_code)
@@ -62,19 +65,25 @@ def find_new_functions(problems, functions, layer, f, verbose, append_functions_
 
 if __name__ == "__main__":
     seed = int(sys.argv[1]) if len(sys.argv) > 1 else 142
-    if random.random() < 0.1:
-        print(seed)
-    verbose = 0
+    output_folder = sys.argv[2] if len(sys.argv) > 2 else "tmp"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    param_file = sys.argv[3] if len(sys.argv) > 3 else "params.txt"
+    with open(param_file, "r") as f:
+        params = json.load(f)
+    with open(f"{output_folder}/params.txt", "w") as f:
+        # write a copy to the output folder
+        json.dump(params, f, sort_keys=True, indent=4)
     random.seed(seed)
     np.random.seed(seed)
-    with open(f"tmp/log_{seed}.txt", "w") as f:
-        functions_file_name = sys.argv[2] if len(sys.argv) > 2 else "functions.txt"
-        problems_file_name = sys.argv[3] if len(sys.argv) > 3 else "problems.txt"
+    with open(f"{output_folder}/log_{seed}.txt", "w") as f:
+        functions_file_name = sys.argv[4] if len(sys.argv) > 4 else "functions.txt"
+        problems_file_name = sys.argv[5] if len(sys.argv) > 5 else "problems.txt"
         functions = interpret.get_functions(functions_file_name)
         problems = interpret.compile(interpret.load(problems_file_name))
         t0 = time.time()
         max_layer = max([problem[-1] for problem in problems])
         for layer in range(1, max_layer+1):
-            find_new_functions(problems, functions, layer, f, verbose, append_functions_to_file=None)
+            find_new_functions(problems, functions, layer, f, params, append_functions_to_file=None)
         t1 = time.time()
         f.write(f"total execution time {int(t1 - t0)} seconds, total evaluations {ga_search_deap.total_eval_count}\n")
