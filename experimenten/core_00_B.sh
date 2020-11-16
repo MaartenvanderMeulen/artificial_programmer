2b9288b1f221e4c2c23e59a36525881ce81630fe
MaartenvanderMeulen <maarten.vandermeulen@gmail.com>
2020-11-16 20:08:27 +0100

    fixed '.model_eval not stored' bug, fixed max_eval bug.  63% on AB, 11''

--- experimenten/core_00_B.sh
+++ experimenten/core_00_B.sh
@@ -1,2 +1,2 @@
-/c/Users/vdmeulem/Miniconda3/python -O search.py 104 tmp/$1 experimenten/params_$1.txt
-/c/Users/vdmeulem/Miniconda3/python -O search.py 108 tmp/$1  experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 104 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 108 tmp/$1  experimenten/params_$1.txt
--- experimenten/core_03_B.sh
+++ experimenten/core_03_B.sh
@@ -1,2 +1,2 @@
-/c/Users/vdmeulem/Miniconda3/python -O search.py 107 tmp/$1 experimenten/params_$1.txt
-/c/Users/vdmeulem/Miniconda3/python -O search.py 111 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 107 tmp/$1 experimenten/params_$1.txt
+/c/Users/Maarten/Miniconda3/python -O search.py 111 tmp/$1 experimenten/params_$1.txt
