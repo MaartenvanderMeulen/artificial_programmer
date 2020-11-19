d0776222b63572585a199daa62b1b4a2a0feb7e8
MaartenvanderMeulen <maarten.vandermeulen@gmail.com>
2020-11-19 22:38:54 +0100

    on nuc just do 2 runs.

--- run_2threads.sh
+++ run_2threads.sh
@@ -3,16 +3,8 @@ for id in $*
 do
     rm -rf tmp/$id
     mkdir -p tmp/$id
-    for part in A B C D
-    do
-        ./experimenten/core_00_$part.sh $id &
-        ./experimenten/core_01_$part.sh $id &
-        wait
-        ./experimenten/core_02_$part.sh $id &
-        ./experimenten/core_03_$part.sh $id &
-        wait
-        echo $id part $part `date` `grep solved tmp/$id/log*.txt | wc --lines` >> tmp/$id/resultaat_$part.$id.txt
-        cat tmp/$id/resultaat_$part.$id.txt        
-    done
-    cat tmp/$id/resultaat_D.$id.txt >> experimenten/resultaat_$id.txt
+    /c/Users/Maarten/Miniconda3/python search.py 100 experimenten/params_$id.txt &
+    /c/Users/Maarten/Miniconda3/python search.py 101 experimenten/params_$id.txt &
+    wait
+    echo $id `date` `grep solved tmp/$id/log*.txt | wc --lines`
 done
