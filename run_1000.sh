24cb3552f0454dd14aa37ad652afb92dc2e4c9cb
MaartenvanderMeulen <maarten.vandermeulen@gmail.com>
2020-12-13 18:14:55 +0100

    spreiding van piek memory gebruik via gespreid opstarten

--- run_1000.sh
+++ run_1000.sh
@@ -7,4 +7,5 @@ for t in `seq 0 1 $lastt`
 do
     startseed=`expr 1000 + $t`
     for seed in `seq $startseed $nt 1999` ; do python solve_problems.py $seed experimenten/params_$id.txt ; done &
+    sleep 50
 done
--- run_all_threads_once.sh
+++ run_all_threads_once.sh
@@ -3,4 +3,5 @@ mkdir -p tmp/$1
 for t in `seq 0 1 23`
 do
     python solve_problems.py `expr 1000 + $t` experimenten/params_$1.txt &
+    sleep 50
 done
