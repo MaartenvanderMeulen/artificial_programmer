2670b6e499eaf9f0c0eaf00331428eb053929c05
MaartenvanderMeulen <maarten.vandermeulen@gmail.com>
2020-12-31 16:12:02 +0100

    09ACV experiment

--- run_all_threads_once.sh
+++ run_all_threads_once.sh
@@ -3,5 +3,5 @@ id=$1
 mkdir -p tmp/$id
 for seed in `seq 1000 1 1029`
 do
-    tsp -n -L $seed python solve_problems.py $seed experimenten/params_$id.txt
+    tsp -n -L $seed python solve_problems.py $seed $id
 done
